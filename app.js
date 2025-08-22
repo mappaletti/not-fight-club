75883bbafce21e01fde9e7aace06ad3388ad30ee
mappaletti <maxjija15@gmail.com>
2025-08-22 23:15:40 +0300

    feat: add main-character-settings swapping

--- app.js
+++ app.js
@@ -4,17 +4,46 @@ export function mount(container) {
             <div class="header-right-side">
                 <nav>
                     <ul>
-                        <li><a href="#"><img src="assets/icons/BTNHoldPosition.webp" alt="main"></a></li>
-                        <li><a href="#"><img src="assets/icons/BTNSleep.webp" alt="character"></a></li>
-                        <li><a href="#"><img src="assets/icons/BTNHumanBuild.webp" alt="settings"></a></li>
+                        <li><a href="#" data-view="main"><img src="assets/icons/BTNHoldPosition.webp" alt="main"></a></li>
+                        <li><a href="#" data-view="character"><img src="assets/icons/BTNSleep.webp" alt="character"></a></li>
+                        <li><a href="#" data-view="settings"><img src="assets/icons/BTNHumanBuild.webp" alt="settings"></a></li>
                     </ul>
                 </nav>
             </div>
         </header>
 
         <div class="content flexcenter">
-            <button id="fight-button">FIGHT!</button>
+            
         </div>`;
 
     container.innerHTML = mainAppHtml;
+    const content = container.querySelector(".content");
+
+    function renderView(view) {
+        content.innerHTML = "";
+
+        if (view === "main") {
+            content.innerHTML = `<button id="fight-button">FIGHT!</button>`
+            return;
+        }
+
+        if (view === "character") {
+            content.innerHTML = `<h1>character</h1>`
+            return;
+        }
+
+        if (view === 'settings') {
+            content.innerHTML = `<h1>settings</h1>`
+            return;
+        }
+    }
+
+    container.addEventListener("click", (e) => {
+        const a = e.target.closest('a[data-view]');
+        if (!a) return;
+        e.preventDefault();
+        renderView(a.dataset.view);
+    });
+
+    renderView("main");
 }
